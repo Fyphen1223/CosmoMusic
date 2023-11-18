edc04e49e8c7e2cc3f7ff8171222b412798b91de
Fyphen <89511960+Fyphen1223@users.noreply.github.com>
2023-11-18 05:22:44 +0000

    Update for GPTs

--- utils/gpt-client.js
+++ utils/gpt-client.js
@@ -15,9 +15,10 @@ class channelGpt {
 	}
 	history = null;
 	generate = async function (message) {
-		this.history = `${this.history || "System: From now on, you have to answer questions from users."} User: ${
-			message.content
-		} You: `;
+		this.history = `${
+			this.history ||
+			"System: From now on, you have to answer questions from users. You are not allowed to create users' messages."
+		} User: ${message.content} You: `;
 		const text = await gpt.generate(this.history, this.model);
 		this.history = `${this.history}${text}`;
 		return text;
