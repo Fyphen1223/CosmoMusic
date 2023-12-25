d7245cd3031df55885658c742ee2d7f9f90e4994
ThePedroo <pedroolimpioguerra@gmail.com>
2023-12-25 02:03:29 -0300

    improve: syntax and overall code
    This commit improves the code's syntax and quality.

--- utils/db-client.js
+++ utils/db-client.js
@@ -1,17 +1,19 @@
-const { createClient } = require("redis");
-const config = require("../config.json");
+const config = require('../config.json');
 
-async function main() {
+const { createClient } = require('redis');
+
+(async () => {
 	const redisClient = await createClient({
 		password: config.db.password,
 		socket: {
 			host: config.db.host,
-			port: config.db.port,
-		},
+			port: config.db.port
+		}
 	})
-		.on("error", (err) => console.log("Redis Client Error", err))
-		.connect();
-	await redisClient.set("test", JSON.stringify({ premium: false, ilegal: false, expires: false }));
-	console.log(JSON.parse(await redisClient.get("test")));
-}
-main();
+	.on('error', (err) => console.log('Redis Client Error', err))
+	.connect();
+
+	await redisClient.set('test', JSON.stringify({ premium: false, ilegal: false, expires: false }));
+
+	console.log(JSON.parse(await redisClient.get('test')));
+})();
