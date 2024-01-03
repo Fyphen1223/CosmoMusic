bb8d41facad10b96dcdf017d4cf6f58a160d964f
Fyphen1223 <fyphensub@gmail.com>
2024-01-03 15:26:27 +0900

    Revert "Revert "Improve code syntax and code quality""
    This reverts commit 5323f0c75f6f167f89bc2d77c8d62890eeafdfeb.

--- utils/gpt-client.js
+++ utils/gpt-client.js
@@ -1,10 +1,8 @@
-const gpt = require("./gpt-api-client.js");
-const config = require("../config.json");
+const gpt = require('./gpt-api-client.js');
 
 class gpts {
 	add(channelId, model) {
 		this[channelId] = new channelGpt(channelId, model);
-		return;
 	}
 }
 
@@ -12,14 +10,21 @@ class channelGpt {
 	constructor(channelId, model) {
 		this.channelId = channelId;
 		this.model = model;
+		this.history = null;
 	}
-	history = null;
-	generate = async function (message) {
-		this.history = `${this.history || "System: From now on, you have to answer questions from users. You are not allowed to create users' messages."} User: ${message.content} You: `;
+
+    async generate(message) {
+		this.history = `${this.history || 'System: From now on, you have to answer questions from users. You are not allowed to create user messages.'} User: ${message.content} You: `;
+
 		const text = await gpt.generate(this.history, this.model);
+
 		this.history = `${this.history}${text}`;
+
 		return text;
-	};
+    };
 }
 
-module.exports = { gpts, channelGpt };
+module.exports = {
+	gpts,
+	channelGpt
+};
